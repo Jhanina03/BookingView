96296a93ab6b3443553ebf56444edf8e11677f9e
¨Dayle <garciadayle2004@gmail.com>
2025-09-01 17:07:29 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
+
+const app=express();
+const PORT=process.env.PORT || 3000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true 
+}));
+
+app.get('/',(req,res)=>{
+    res.send('API working');
+});
+app.use('/api/auth',authRouter)
+
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
