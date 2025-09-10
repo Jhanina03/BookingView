b45c822da37a2033ef09b0346f547c5c466218f4
¨Dayle <garciadayle2004@gmail.com>
2025-09-09 21:24:29 -0500

    Implementación de frontend de autentificación y arreglo de backend

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-const app=express();
-const PORT=process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 connectDB();
+
+const allowedOrigins = ["http://localhost:5173"];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true 
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-app.get('/',(req,res)=>{
-    res.send('API working');
+app.get("/", (req, res) => {
+  res.send("API working");
 });
-app.use('/api/auth',authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
