f7e536e5d87595905bab62880a45b839bdadbb50
Pablo Montero <monteropablo511@gmail.com>
2025-09-13 09:20:05 -0500

    home pages

--- frontend/src/components/FeauteredDestination.jsx
+++ frontend/src/components/FeauteredDestination.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { roomsDummyData } from '../assets/assets'
-import HotelCard from '../components/HotelCard'
+import React from "react";
+import { roomsDummyData } from "../assets/assets";
+import HotelCard from "../components/HotelCard";
+import Title from "./Title";
+import { useNavigate } from "react-router-dom";
 
 const FeauteredDestination = () => {
-  return (
-    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50'>
-        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-            {roomsDummyData.slice(0,4).map((room, index) => (<HotelCard key={room._id} room={room} index={index}></HotelCard>))}
-        </div>
-      
-    </div>
-  )
-}
 
-export default FeauteredDestination
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex flex-col items-center mt-10 px-6 md:px-16 lg:px-24 bg-slate-50">
+      <Title
+        title="Featured Destination"
+        subTitle="Discover our handpicked section of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences."
+      />
+
+      <div className="flex flex-wrap items-center justify-center gap-6 mt-10">
+        {roomsDummyData.slice(0, 4).map((room, index) => (
+          <HotelCard key={room._id} room={room} index={index}></HotelCard>
+        ))}
+      </div>
+
+      <button onClick={() => {navigate('/rooms'); scrollTo(0,0)}} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+          View All Destinations
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default FeauteredDestination;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeauteredDestination from '../components/FeauteredDestination'
+import ExcusiveOffers from '../components/ExcusiveOffers'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <FeauteredDestination/>
+      <ExcusiveOffers/>
+      <Testimonial/>
     </>
   )
 }
