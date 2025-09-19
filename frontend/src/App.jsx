952aef87e6039595196630803afff07387c69022
Sebas Constante <sebastianconstante04@hotmail.com>
2025-09-18 21:13:47 -0500

    Dashboard for room owners made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import Home from "./pages/Home";
 import Footer from "./components/footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
 
 const App = () => {
 
@@ -12,15 +17,21 @@ const App = () => {
 
   return (
     <div>
-      {!isOwnerPath && <Navbar/>}
+      {!isOwnerPath && <Navbar />}
+      {/* {false && <HotelReg />} */}
       <div className="min-h-[70vh]">
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/rooms" element={<AllRooms/>}/>
-          <Route path="/rooms/:id" element={<RoomDetails/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+import logo from './logoActual.svg'
 import searchIcon from './searchIcon.svg'
 import userIcon from './userIcon.svg'
 import calenderIcon from './calenderIcon.svg'
