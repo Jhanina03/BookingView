3ff1835dd42f82d8a54182b1cb54c10ea9e8ae27
¨Dayle <garciadayle2004@gmail.com>
2025-09-21 00:45:09 -0500

    Remove node modules from remote

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userAuth from '../middlewares/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
-const userRouter = express.Router();
-
-userRouter.get('/data',userAuth,getUserData);
-
-export default userRouter;
