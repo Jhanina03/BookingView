73a4587519af33e48ae4d2badbbc4c9c2019be73
Jhani <jhaninaconteron03@gmail.com>
2025-09-26 23:07:06 -0500

    Cambios para Vercel

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -137,3 +137,17 @@ export const getHotelBookings = async (req, res) => {
     res.json({ success: false, message: "Failed to get bookings" });
   }
 };
+
+export const stripePayment = async (req, res) => {
+  try {
+    const { bookingId } = req.body;
+
+    const booking = await Booking.findById(bookingId);
+    const roomData = await Room.findById(booking.room).populate('hotel');
+    const totalPrice = booking.totalPrice;
+    const {origin} = req.headers;
+    
+  } catch (error) {
+
+  }
+}
--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -15,23 +15,36 @@ const clerkWebhooks = async (req, res) => {
 
     const { data, type } = req.body;
 
-    const userData = {
-      _id: data.id,
-      email: data.email_addresses[0].email_addresses,
-      username: data.first_name + " " + data.last_name,
-      image: data.image_url,
-    };
+
 
     switch (type) {
       case "user.created": {
+            const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_addresses,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url,
+          };
         await User.create(userData);
         break;
       }
       case "user.updated": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_addresses,
+          username: data.first_name + " " + data.last_name,
+          image: data.image_url,
+        };        
         await User.findByIdAndUpdate(data.id, userData);
         break;
       }
       case "user.deleted": {
+      const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_addresses,
+        username: data.first_name + " " + data.last_name,
+        image: data.image_url,
+      };
         await User.findByIdAndDelete(data.id);
         break;
       }
