582e52a4336b086ef114a48b18abffb41ef6fa50
Jhani <jhaninaconteron03@gmail.com>
2025-10-10 00:11:36 -0500

    Cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { React } from "react";
-import Navbar from "./components/Navbar";
+import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
+import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
@@ -11,40 +11,50 @@ import Layout from "./pages/hotelOwner/Layout";
 import Dashboard from "./pages/hotelOwner/Dashboard";
 import AddRoom from "./pages/hotelOwner/AddRoom";
 import ListRoom from "./pages/hotelOwner/ListRoom";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { useAppContext } from "./context/appContext";
 import Loader from "./components/Loader";
+import ReactivateAccount from "./pages/ReactivateAccount";
 
 const App = () => {
-
   const isOwnerPath = useLocation().pathname.includes("owner");
-  const {showHotelReg} = useAppContext();
-  // const {showHotelReg} = useAppContext();
+  const { showHotelReg, isLoading, isInactive } = useAppContext();
 
   return (
     <div>
-      <Toaster/>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
       {showHotelReg && <HotelReg />}
-      {/* {false && <HotelReg />} */}
+      
+      {isInactive && (
+        <div className="p-4 bg-yellow-100 text-yellow-800 text-center">
+          Tu cuenta está inactiva. <a href="/reactivate" className="underline">Reactivar ahora</a>
+        </div>
+      )}
+
       <div className="min-h-[70vh]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<AllRooms />} />
-          <Route path="/rooms/:id" element={<RoomDetails />} />
-          <Route path="/my-bookings" element={<MyBookings/>}/>
-          <Route path="/loader/:nextUrl" element={<Loader/>}/>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<AllRooms />} />
+            <Route path="/rooms/:id" element={<RoomDetails />} />
+            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/loader/:nextUrl" element={<Loader />} />
             <Route path="/reactivate" element={<ReactivateAccount />} />
-          <Route path="/owner" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="add-room" element={<AddRoom />} />
-            <Route path="list-room" element={<ListRoom />} />
-          </Route>
-        </Routes>
+            <Route path="/owner" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="add-room" element={<AddRoom />} />
+              <Route path="list-room" element={<ListRoom />} />
+            </Route>
+          </Routes>
+        )}
       </div>
+
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
